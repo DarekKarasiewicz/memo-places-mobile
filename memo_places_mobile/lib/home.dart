63f095e9772ae83f6bef72cc2e2ae6fcb50bde31
Mikolajkusinski <mikolajk558@gmail.com>
2024-04-06 16:10:27 +0200

    Added Google Map
    Added map and current user location;
    Secured API Key.

--- memo_places_mobile/lib/home.dart
+++ memo_places_mobile/lib/home.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
-  
+
   @override
-  _HomeSate createState() => _HomeSate();
+  State createState() => _GoogleMapsState();
 }
 
-class _HomeSate extends State<Home> {
+class _GoogleMapsState extends State {
+  late GoogleMapController mapController;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   String? _access;
+  late LatLng _position;
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation().then((location) => setState(() {
+          _position = LatLng(location.latitude, location.longitude);
+        }));
+  }
 
   Future<String?> _loadCounter(String key) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -23,17 +36,44 @@ class _HomeSate extends State<Home> {
     prefs.setString(key, value);
   }
 
+  Future<Position> _getCurrentLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied');
+    }
+
+    isLoading = false;
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.lightBlue,
-            title: const Text("Home")
-            ),
-      body: Center(
-            child: Text("Home", style: TextStyle(fontSize: 60),)),
+            backgroundColor: Colors.lightBlue, title: const Text("Home")),
+        body: Center(
+          child: isLoading
+              ? Text("Loading", style: TextStyle(fontSize: 60))
+              : GoogleMap(
+                  onMapCreated: _onMapCreated,
+                  initialCameraPosition:
+                      CameraPosition(target: _position, zoom: 12.0),
+                ),
+        ),
       ),
     );
   }
